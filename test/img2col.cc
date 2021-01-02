451db878e4b45de3452d8a5a5e25504383db1988
Feng Wang <wang_feng@live.com>
2021-01-02 22:57:06 +0100

    img2col stride tested

--- test/img2col.cc
+++ test/img2col.cc
@@ -85,11 +85,54 @@ void test_3332_33()
     std::cout << "after col2img:\n" << col_im << std::endl;
 }
 
+void test_1441_22_s2()
+{
+    auto a = ceras::linspace<double>( 1.0, 16.0, 16 );
+    std::cout << "a created with:\n" << a << std::endl;
+    a.reshape( {1, 4, 4, 1} );
+
+    auto va = ceras::variable<double>{ a };
+    auto col_va = ceras::img2col(2, 2, 0, 0, 2, 2)( va );
+
+    ceras::session<double> s;
+    auto col_im = s.run( col_va );
+    std::cout << "after col2img:\n" << col_im << std::endl;
+}
+
+void test_1442_22_s2()
+{
+    auto a = ceras::linspace<double>( 1.0, 32.0, 32 );
+    std::cout << "a created with:\n" << a << std::endl;
+    a.reshape( {1, 4, 4, 2} );
+
+    auto va = ceras::variable<double>{ a };
+    auto col_va = ceras::img2col(2, 2, 0, 0, 2, 2)( va );
+
+    ceras::session<double> s;
+    auto col_im = s.run( col_va );
+    std::cout << "after col2img:\n" << col_im << std::endl;
+}
+
+void test_2442_22_s2()
+{
+    auto a = ceras::linspace<double>( 1.0, 32.0*2, 32*2 );
+    std::cout << "a created with:\n" << a << std::endl;
+    a.reshape( {2, 4, 4, 2} );
+
+    auto va = ceras::variable<double>{ a };
+    auto col_va = ceras::img2col(2, 2, 0, 0, 2, 2)( va );
+
+    ceras::session<double> s;
+    auto col_im = s.run( col_va );
+    std::cout << "after col2img:\n" << col_im << std::endl;
+}
+
 
 int main()
 {
     using namespace ceras;
 
+#if 0
     test_55_33();
     test_3320_33();
 
@@ -101,7 +144,14 @@ int main()
 
     std::cout << ">---------------<" << std::endl;
     test_2332_33();
+#endif
 
+    std::cout << "Test Stride" << std::endl;
+    test_1441_22_s2();
+
+    test_1442_22_s2();
+
+    test_2442_22_s2();
 
     return 0;
 }
