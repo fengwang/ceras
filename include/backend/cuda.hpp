c25680459414ea5e48690baa633243737d79d452
Feng Wang <wang_feng@live.com>
2021-01-18 20:15:14 +0100

    optimized memcpy

--- include/backend/cuda.hpp
+++ include/backend/cuda.hpp
@@ -361,7 +361,8 @@ namespace ceras
         T* b = a + m*n;
         host_to_device_n( B, n*k, b );
         T* c = b + n*k;
-        host_to_device_n( C, m*k, c );
+        // not copying this
+        //host_to_device_n( C, m*k, c );
 
         T* result_ptr = c;
         T* first_ptr = b;
