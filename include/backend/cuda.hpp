cf5caccc3e68391b2be498f2df0adacca8f9b87f
Feng Wang <wang_feng@live.com>
2021-01-18 14:40:25 +0100

    acknowledges to caffe.

--- include/backend/cuda.hpp
+++ include/backend/cuda.hpp
@@ -883,33 +883,5 @@ namespace ceras
 
 }
 
-/*
-template <>
-void caffe_gpu_gemm<float>(const cublasOperation_t TransA, const cublasOperation_t TransB, const int M, const int N, const int K, const float alpha, const float* A, const float* B, const float beta, float* C) {
-  // Note that cublas follows fortran order.
-  int lda = (TransA == CUBLAS_OP_N) ? K : M;
-  int ldb = (TransB == CUBLAS_OP_N) ? N : K;
-  cublasOperation_t cuTransA = (TransA == CUBLAS_OP_N) ? CUBLAS_OP_N : CUBLAS_OP_T;
-  cublasOperation_t cuTransB = (TransB == CUBLAS_OP_N) ? CUBLAS_OP_N : CUBLAS_OP_T;
-  CUBLAS_CHECK(cublasSgemm_v2(Caffe::cublas_handle(), cuTransB, cuTransA, N, M, K, &alpha, B, ldb, A, lda, &beta, C, N));
-}
-
-template <>
-void caffe_gpu_gemm<double>(const cublasOperation_t TransA,
-    const cublasOperation_t TransB, const int M, const int N, const int K,
-    const double alpha, const double* A, const double* B, const double beta,
-    double* C) {
-  // Note that cublas follows fortran order.
-  int lda = (TransA == CUBLAS_OP_N) ? K : M;
-  int ldb = (TransB == CUBLAS_OP_N) ? N : K;
-  cublasOperation_t cuTransA =
-      (TransA == CUBLAS_OP_N) ? CUBLAS_OP_N : CUBLAS_OP_T;
-  cublasOperation_t cuTransB =
-      (TransB == CUBLAS_OP_N) ? CUBLAS_OP_N : CUBLAS_OP_T;
-  CUBLAS_CHECK(cublasDgemm_v2(Caffe::cublas_handle(), cuTransB, cuTransA,
-      N, M, K, &alpha, B, ldb, A, lda, &beta, C, N));
-}
-*/
-
 #endif//HLUDCUYMXCGREGWXHAVFNPYCNJLENNKSIOVPKKRXDUBYCAEMYBOKVKWODFGLJOUYYLQEDWSTB
 
