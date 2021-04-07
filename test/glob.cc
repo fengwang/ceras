91b1cea14a822a11657f7e344119a32f0472669f
Feng Wang <wang_feng@live.com>
2021-04-07 22:06:41 +0200

    updated glob example

--- test/glob.cc
+++ test/glob.cc
@@ -5,8 +5,17 @@ int main()
 {
     auto paths = glob::glob("./test/*.cc");
     std::cout << "There are " << paths.size() << " cpp source files under test folder:\n";
-    for ( auto path : paths )
+    for ( auto const& path : paths )
         std::cout << path.string() << std::endl;
+
+    {
+        auto paths = glob::glob("./examples/vgg16/dogs-vs-cats/train/cat.*.jpg");
+        std::cout << "There are " << paths.size() << " cat images\n";
+    }
+    {
+        auto paths = glob::glob("./examples/vgg16/dogs-vs-cats/train/dog.*.jpg");
+        std::cout << "There are " << paths.size() << " dog images\n";
+    }
 
     return 0;
 }
