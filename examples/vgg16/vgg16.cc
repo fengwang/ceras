69ba95d7f0ee1dea3febdf78975eea60d370b533
Feng Wang <wang_feng@live.com>
2021-04-19 22:50:24 +0200

    udpated vgg16 code

--- examples/vgg16/vgg16.cc
+++ examples/vgg16/vgg16.cc
@@ -15,8 +15,6 @@ typedef tensor<float> tensor_type;
 
 int main()
 {
-    random_generator.seed( 42 ); // just for reproducibility
-
     auto input = Input(); //  3D tensor input, (batch_size, 224, 224, 3)
     auto l0 = relu( Conv2D( 64, {3, 3}, {224, 3, 3}, "same" )(input) ); // 224, 224, 64
     auto l1 = max_pooling_2d( 2 ) ( l0 ); // 112, 112, 64
@@ -41,101 +39,14 @@ int main()
     auto l20 = relu( Dense( 1000, 4096 )( l19 ) ); // 1000
     auto output = l20;
 
+    auto m = model{ input, output }; // define a model
+
     auto ground_truth = place_holder<tensor_type>{}; // 1-D, 1000
     auto loss = cross_entropy_loss( ground_truth, output );
 
 
-
-
     #if 0
-    // preparing training
-    std::size_t const batch_size = 10;
-    tensor_type input_images{ {batch_size, 224, 224, 3} };
-    tensor_type output_labels{ {batch_size, 1000} };
-
-    //std::size_t const epoch = 100;
-    std::size_t const epoch = 1;
-    std::size_t const iteration_per_epoch = 60000/batch_size;
-
-    // creating session
-    session<tensor_type> s;
-    s.bind( input, input_images );
-    s.bind( ground_truth, output_labels );
-
-    // proceed training
-    float learning_rate = 1.0e-1f;
-    //float learning_rate = 1.0e-1f;
-    //float learning_rate = 5.0e-1f;
-    auto optimizer = gradient_descent{ loss, batch_size, learning_rate };
-
-    for ( auto e : range( epoch ) )
-    {
-
-        for ( auto i : range( iteration_per_epoch ) )
-        {
-            // generate images
-            std::size_t const image_offset = 16 + i * batch_size * 28 * 28;
-            for ( auto j : range( batch_size * 28 * 28 ) )
-                input_images[j] = static_cast<float>(training_images[j+image_offset]) / 127.5f - 1.0f;
-            better_assert( !has_nan( input_images ), "input_images has nan at iteration ", i );
-
-            // generating labels
-            std::size_t const label_offset = 8 + i * batch_size * 1;
-            std::fill_n( output_labels.data(), output_labels.size(), 0.0f ); //reset
-            for ( auto j : range( batch_size * 1 ) )
-            {
-                std::size_t const label = static_cast<std::size_t>(training_labels[j+label_offset]);
-                output_labels[j*10+label] = 1.0f;
-            }
-            better_assert( !has_nan( output_labels ), "output_labels has nan at iteration ", i );
-
-            auto current_error = s.run( loss );
-            std::cout << "Loss at epoch " << e << " index: " << (i+1)*batch_size << ":\t" << current_error[0] << "\r" << std::flush;
-            better_assert( !has_nan(current_error), "Error in current loss." );
-            s.run( optimizer );
-        }
-        std::cout << std::endl;
-    }
-
-    std::cout << std::endl;
-
-
-    unsigned long const new_batch_size = 1;
-
-    std::vector<std::uint8_t> testing_images = load_binary( testing_image_path );
-    std::vector<std::uint8_t> testing_labels = load_binary( testing_label_path );
-    std::size_t const testing_iterations = 10000 / new_batch_size;
-
-    tensor<float> new_input_images{ {new_batch_size, 28 * 28} };
-    s.bind( input, new_input_images );
-
-    unsigned long errors = 0;
-
-    for ( auto i = 0UL; i != testing_iterations; ++i )
-    {
-        std::size_t const image_offset = 16 + i * new_batch_size * 28 * 28;
-
-        for ( auto j = 0UL; j != new_batch_size*28*28; ++j )
-            new_input_images[j] = static_cast<float>( testing_images[j + image_offset] ) / 127.5f - 1.0f;
-
-        auto prediction = s.run( output );
-        prediction.reshape( {prediction.size(), } );
-        std::size_t const predicted_number = std::max_element( prediction.begin(), prediction.end() ) - prediction.begin();
-
-        std::size_t const label_offset = 8 + i * new_batch_size * 1;
-        std::size_t const ground_truth = testing_labels[label_offset];
-
-        if ( predicted_number != ground_truth )
-        {
-            errors += 1;
-            std::cout << "Prediction error at " << i << ": predicted " << predicted_number << ", but the ground_truth is " << ground_truth << std::endl;
-        }
-
-    }
-
-    float const err = 1.0 * errors / 10000;
-    std::cout << "Prediction error on the testing set is " << err << std::endl;
-
+    training code ommited.
     #endif
 
     return 0;
