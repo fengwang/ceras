9a21738ef40ecc1ffe667e5e16a51e8917a01933
Feng Wang <wang_feng@live.com>
2021-06-16 23:12:51 +0200

    tmp commit

--- test/dcgan.cc
+++ test/dcgan.cc
@@ -5,20 +5,6 @@
 //using namespace ceras;
 auto build_generator( unsigned long const latent_dim )
 {
-#if 0
-    auto input = ceras::Input(); // (latent_dim, )
-    auto l1 = ceras::ReLU( ceras::Dense( 128*7*7, latent_dim )( input ) );
-    auto l2 = ceras::Reshape( {7, 7, 128} )( l1 );
-    auto l3 = ceras::UpSampling2D( 2 )( l2 ); // (14, 14, 128)
-    auto l4 = ceras::Conv2D( 128, {3, 3}, {14, 14, 128}, "same" )( l3 );
-    auto l5 = ceras::ReLU( ceras::BatchNormalization( 0.8f, {14, 14, 128} )( l4 ) );
-    auto l6 = ceras::UpSampling2D( 2 )( l5 ); // (28, 28, 128)
-    auto l7 = ceras::Conv2D( 64, {3, 3}, {28, 28, 128}, "same" )( l6 );
-    auto l8 = ceras::ReLU( ceras::BatchNormalization( 0.8f, {28, 28, 64} )( l7 ) );
-    auto output = ceras::tanh( ceras::Conv2D( 1, {3,3}, {28, 28, 64}, "same" )( l8 ) );
-    return ceras::model{ input, output };
-#endif
-
     auto input = ceras::Input(); // (latent_dim, )
     auto l0 = ceras::ReLU( ceras::Dense( 1024, latent_dim )( input ) );
     auto l1 = ceras::ReLU( ceras::Dense( 1024, 1024 )( l0 ) );
@@ -30,16 +16,6 @@ auto build_generator( unsigned long const latent_dim )
 
 auto build_discriminator()
 {
-#if 0
-    auto input = ceras::Input(); // (28, 28, 1)
-    auto l1 = ceras::MaxPooling2D(2)( ceras::Dropout(0.25f)( ceras::LeakyReLU(0.2f)( ceras::Conv2D( 32, {3, 3}, {28, 28, 1}, "same" )( input ) ) ) ); // (14, 14, 32)
-    auto l2 = ceras::MaxPooling2D(2)( ceras::Dropout(0.25f)( ceras::LeakyReLU(0.2f)( ceras::BatchNormalization(0.8f, {14, 14, 64})( ceras::LeakyReLU(0.2f)( ceras::Conv2D( 64, {3, 3}, {14, 14, 32}, "same" )( l1 ) ) ) ) ) ); //(7, 7, 64)
-    auto l3 = ceras::Flatten()( l2 ); //(7*7*64)
-    auto l4 = ceras::Dropout(0.25f)( ceras::LeakyReLU(0.2f)( ceras::Dense( 64, 7*7*64 )( l3 ) ) );
-    auto output = ceras::sigmoid( ceras::Dense( 1, 64 )( l4 ) );
-    return ceras::model{ input, output };
-#endif
-
     auto input = ceras::Input(); // (28, 28, 1)
     auto l0 = ceras::Flatten()( input );
     auto l1 = ceras::ReLU( ceras::Dense( 1024, 28*28 )( l0 ) );
@@ -54,15 +30,24 @@ int main()
 
     unsigned long const latent_dim = 16;
     unsigned long const epochs = 20;
-    unsigned long const batch_size = 600; // should work
+    unsigned long const batch_size = 10; // should work
     unsigned long const iterations = 60000 / batch_size;
 
     // build models
     auto discriminator = build_discriminator();
+
     auto generator = build_generator( latent_dim );
+
     auto z = ceras::Input(); // (latent_dim, )
-    auto critic = discriminator( generator( z ) );
+    auto dis = discriminator;
+    dis.trainable(false);
+    auto critic = dis( generator( z ) );
     auto combined = ceras::model{ z, critic };
+
+    combined.summary( "./dcgan_combined.dot" );
+    discriminator.summary( "./dcgan_discriminator.dot" );
+    generator.summary( "./dcgan_generator.dot" );
+
     ceras::debug_log( "All models generated." );
 
     auto c = combined.compile( ceras::MeanAbsoluteError(), ceras::SGD( batch_size, 0.05f ) ); //
@@ -85,17 +70,23 @@ int main()
             auto const& noise = ceras::randn<float>( {batch_size, latent_dim} );
             auto const& gen_images = generator.predict( noise );
 
-            d.trainable( true );
+            //d.trainable( true );
             auto loss_fake = d.train_on_batch( gen_images, fake );
             std::copy_n( X.begin()+idx*batch_size*28*28*1, batch_size*28*28*1, cache.begin() );
             auto loss_valid = d.train_on_batch( cache, valid );
 
-            d.trainable( false );
+            //d.trainable( false );
             auto loss_gan = c.train_on_batch( noise, valid );
 
             std::cout << "At epoch " << e << " iteration " << idx << ", the losses are " << loss_fake << " " << loss_valid << " " << loss_gan <<  "\r" << std::flush;
         }
         std::cout << std::endl;
+
+        auto const& noise = ceras::randn<float>( {1, latent_dim} );
+        auto gen_images = generator.predict( noise );
+        gen_images += 1.0f;
+        gen_images *= 127.5f;
+        ceras::imageio::imwrite( std::string{"./tmp/dcgan_example_"}+std::to_string(e)+std::string{".png"}, ceras::squeeze( gen_images ) );
     }
 
     generator.save_weights( "./tmp/dcgan" );
